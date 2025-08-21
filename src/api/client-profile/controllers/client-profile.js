f4d3b1b8359d99cd0ab85fbe28e809c6a4ec45c2
Khaled <khaled.mourraad@gmail.com>
2025-08-21 12:11:08 +0300

    feat: add client profile schema

---
+++ src/api/client-profile/controllers/client-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * client-profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::client-profile.client-profile');
---
+++ src/api/client-profile/routes/client-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * client-profile router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::client-profile.client-profile');
