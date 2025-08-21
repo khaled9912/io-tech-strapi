556c232a47785f5efd3c9dd39aeec85a5ea59341
Khaled <khaled.mourraad@gmail.com>
2025-08-21 12:12:09 +0300

    feat: add hero section content schema

---
+++ src/api/hero-section-content/routes/hero-section-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hero-section-content router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hero-section-content.hero-section-content');
---
+++ src/api/hero-section-content/services/hero-section-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hero-section-content service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::hero-section-content.hero-section-content');
